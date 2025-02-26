eb2f7f8df113e826591bf8c4deb335fd5b0178ec
owls4all <owls4all@gmail.com>
2025-02-26 12:27:25 -0800

    did

--- q1.py
+++ q1.py
@@ -3,13 +3,14 @@
 # Create a list with the following 5 items:
 # 7, 9, "a", "cat", False
 # Assign this list to the variable my_list. Then print my_list
-
+my_list = [7,9,"a","cat",False]
 #YOUR CODE HERE
-
+print(my_list)
 # b) Append 3.14 and 7 to the list. Then print my_list
-
+my_list.append(3.14)
+my_list.append(7)
 #YOUR CODE HERE
-
+print(my_list)
 # c) Find and print the index of "cat"
-
+print(my_list.index("cat"))
 #YOUR CODE HERE
